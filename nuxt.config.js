6652fc18fb89195bd3749bd13305055abb4fc95f
Artem Vilchinsky <k1zer.X@yandex.ru>
2020-10-01 02:17:09 +0300

    head & meta added, initial-scale fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,17 @@ export default {
 		port: process.env.PORT || 3000, // heroku specifies the port using PORT
 		host: '0.0.0.0',
 	},
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=2' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		]
+	},
 	components: true,
 	buildModules: [
 		'@nuxtjs/vuetify',
