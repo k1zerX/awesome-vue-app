e72cad0eca7849102c13fb246b56fd849c07175c
Artem Vilchinsky <k1zer.X@yandex.ru>
2020-10-01 07:55:49 +0300

    add links to news

--- components/news_article.vue
+++ components/news_article.vue
@@ -1,20 +1,26 @@
 <template>
-	<v-card
-		class="pa-2"
-		tile
-		style="margin: 10px; width: 300px;"
+	<a
+		:href="article.url"
+		style="text-decoration: none;"
 	>
-		<v-img
-			:src="article.urlToImage"
-			max-width="100%"
-		/>
-		<v-card-title>
-			{{article.title}}
-		</v-card-title>
-		<v-card-subtitle>
-			{{article.description}}
-		</v-card-subtitle>
-	</v-card>
+		<v-card
+			class="pa-2"
+			tile
+			style="margin: 10px; width: 300px;"
+			@click=""
+		>
+			<v-img
+				:src="article.urlToImage"
+				max-width="100%"
+			/>
+			<v-card-title>
+				{{article.title}}
+			</v-card-title>
+			<v-card-subtitle>
+				{{article.description}}
+			</v-card-subtitle>
+		</v-card>
+	</a>
 </template>
 
 <script>
